932d2eec405bfa138948c7c1ee886d7b382a93ca
K Vaibhav <kv9976699@gmail.com>
2026-07-24 07:11:19 +0000

    I configure the database and created new data base inside the ubantu and everything is working fine fastapi is connected

---
+++ backend/app/database/base.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+# Import all models here
+from app.models.user import User
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,27 @@
-from fastapi import FastAPI
+from app.database.init_db import init_db
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from app.core.config import settings
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Initializing Database...")
+    init_db()
+    yield
+    print("Application Shutdown")
+
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    lifespan=lifespan
+)
 
 
 @app.get("/")
 def root():
     return {
-        "message": "Illumination AI Backend is Running 🚀"
+        "message": f"Welcome to {settings.APP_NAME} 🚀",
+        "version": settings.APP_VERSION
     }
