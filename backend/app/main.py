bebd32a9b45d94e7285e4e033c4dde6ddd720114
K Vaibhav <kv9976699@gmail.com>
2026-07-29 14:05:28 +0000

    Congratulation we complete this things ✅ Status: 201 ✅ Backend returned the created user object ✅ React → FastAPI communication is working ✅ PostgreSQL insertion is working ✅ Registration API is working end-to-end

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from app.api.auth import router as auth_router
 
 from app.database.init_db import init_db
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI
 from app.core.config import settings
@@ -18,6 +19,15 @@ app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
     lifespan=lifespan
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
