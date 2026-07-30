a6a79e8b949e569d0df45282225ee1eae4245bf6
K Vaibhav <kv9976699@gmail.com>
2026-07-30 14:01:14 +0000

    working on the image preprocessing and new react conditional rendering and fixing and cleaning some backend issues and files and handling the blobs

--- frontend/src/components/UploadCard.jsx
+++ frontend/src/components/UploadCard.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useRef, useState } from "react";
 import "../styles/UploadCard.css";
 
 function UploadCard() {
+  const fileInputRef = useRef(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const previewUrl = selectedImage
+    ? URL.createObjectURL(selectedImage)
+    : null;
+
+  const handleChooseImage = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    setSelectedImage(file);
+
+    console.log(file);
+  };
   return (
     <div className="upload-card">
       <h2>Upload Image</h2>
@@ -10,15 +33,47 @@ function UploadCard() {
         category.
       </p>
 
-      <div className="upload-area">
-        <p>Drag & Drop your image here</p>
+      {
+  selectedImage ? (
+    <div className="upload-area">
+      <img
+        src={previewUrl}
+        alt="Preview"
+        className="image-preview"
+      />
 
-        <span>or</span>
+      <p>{selectedImage.name}</p>
 
-        <button className="upload-button">
-          Choose Image
-        </button>
-      </div>
+      <button
+        className="upload-button"
+        onClick={handleChooseImage}
+      >
+        Change Image
+      </button>
+    </div>
+  ) : (
+    <div className="upload-area">
+      <p>Drag & Drop your image here</p>
+
+      <span>or</span>
+
+      <input
+        type="file"
+        ref={fileInputRef}
+        accept="image/*"
+        hidden
+        onChange={handleImageChange}
+      />
+
+      <button
+        className="upload-button"
+        onClick={handleChooseImage}
+      >
+        Choose Image
+      </button>
+    </div>
+  )
+}
     </div>
   );
 }
