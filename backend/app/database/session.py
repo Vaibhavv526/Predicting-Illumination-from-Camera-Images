df7194727a3d01e0f063fd9a5b15007c551be170
K Vaibhav <kv9976699@gmail.com>
2026-08-01 11:40:10 +0000

    Fix SQLAlchemy connection pool for Neon

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -4,8 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+
 engine = create_engine(
-    settings.DATABASE_URL
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    poolclass=NullPool,
 )
 
 SessionLocal = sessionmaker( #Every incoming request gets its own database session.
