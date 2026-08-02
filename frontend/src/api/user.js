f516118b723c6d53af10bbb06d4f50db0afce1ca
K Vaibhav <kv9976699@gmail.com>
2026-08-02 06:41:39 +0000

    just connecting the frontend with the backend and i think it was the final commit

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL =
+  "https://predicting-illumination-from-camera-images-production.up.railway.app";
 
 export const getProfile = async () => {
   const token = localStorage.getItem("access_token");
