eaa5716e19a0e9c52f16999c9a6a4f3fa16e1109
K Vaibhav <kv9976699@gmail.com>
2026-09-18 00:24:27 +0530

    changing the backend host

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "https://predicting-illumination-from-camera-images-production.up.railway.app";
+  "https://predicting-illumination-from-camera.onrender.com";
 
 export const getProfile = async () => {
   const token = localStorage.getItem("access_token");
